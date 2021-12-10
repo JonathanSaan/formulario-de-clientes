8c51014546e8cb1345d07dafdd85f2481bc6ae28
Jonathan <52434574+JonathanSaan@users.noreply.github.com>
2021-12-10 13:51:15 -0300

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -1,8 +1,16 @@
 function verificar(){
-    var botoes = document.querySelectorAll('input[name="citizenship"]:checked').length;
-    var nascimento = document.getElementById("data_nascimento").value
+    let botoes = document.querySelectorAll('input[name="citizenship"]:checked').length;
+    let nascimento = document.getElementById("data_nascimento").value;
+    let name = document.querySelector('input[name="name"]').value;
+    let email = document.querySelector('input[name="email"]').value;
+    let number = document.querySelector('input[name="number"]').value;
+    let city = document.querySelector('input[name="city"]').value;
+    let state = document.querySelector('input[name="state"]').value;
+    let address = document.querySelector('input[name="address"]').value;
     
-    if (nascimento == "" || botoes == 0){
+    if (nascimento == "" || botoes == 0 || name == "" || email == "" || number == "" || city == "" || state == "" || address == ""){
       window.alert('[ERRO] Verifique os dados e tente novamente')
     }
+    
+   console.log(name)
   }
